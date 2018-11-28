f057a24382feeb947662b2d69ec5bf2dcd1b0beb
Administrator <Administrator@win2012base>
2018-11-28 06:47:43 -0800

    actuator enablement in item service

--- item/src/main/java/com/abcshopping/item/ItemApplication.java
+++ item/src/main/java/com/abcshopping/item/ItemApplication.java
@@ -2,11 +2,10 @@ package com.abcshopping.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class ItemApplication {
 
 	public static void main(String[] args) {
---
+++ item/src/main/java/com/abcshopping/item/ItemSecurityConfig.java
@@ -0,0 +1,14 @@
+package com.abcshopping.item;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ItemSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().antMatchers("/**").permitAll().anyRequest().authenticated();
+		http.headers().frameOptions().sameOrigin();
+	}
+}
